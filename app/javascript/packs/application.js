3adee2d24a46625ab2ea0d056f409358f81284ef
leonardo <leonardoscorzas88@gmail.com>
2018-05-22 15:24:02 -0300

    include vuetify

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+
+Vue.use(Vuetify)
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
